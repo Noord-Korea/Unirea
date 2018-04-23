f56d5b998e79b880c9ceeb07bff64a328961d16a
bramk <bramkempen@gmail.com>
2018-04-23 11:00:17 +0200

    add post handler

--- src/main/java/com/restserver/services/AccountService.java
+++ src/main/java/com/restserver/services/AccountService.java
@@ -1,7 +1,9 @@
 package com.restserver.services;
 
 import com.google.gson.Gson;
+import com.restserver.json.result.Login;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,31 +11,15 @@ import javax.ws.rs.core.Response;
 
 @Path("/account")
 public class AccountService {
-    @POST
-    @Path("/login/{value}")
-    public Response getLogin(@PathParam("value") String msg) {
-        ScoreResponse response = new ScoreResponse();
-        response.setOperation("login");
-        response.setExpression(msg);
+    @POST @Consumes("application/json")
+    @Path("/login")
+    public Response getLogin(Login data) {
+        System.out.println(data.getUsername());
+        System.out.println(data.getPassword());
+
         Gson gson = new Gson();
-        String output = gson.toJson(response);
-        return Response.status(200).entity(output).build();
-    }
-    @POST
-    @Path("/register/{value}")
-    public Response getRegister(@PathParam("value") String msg) {
-        ScoreResponse response = new ScoreResponse();
-        response.setOperation("register");
-        response.setExpression(msg);
-        try {
-            int value = Integer.parseInt(msg);
-            int result = (value<0)? -value:value;
-            response.setResult(Integer.toString(result) + " Ez clap");
-        } catch (NumberFormatException nfe) {
-            response.setResult("invalid value");
-        }
-        Gson gson = new Gson();
-        String output = gson.toJson(response);
+        String output = gson.toJson(data);
+
         return Response.status(200).entity(output).build();
     }
 }
