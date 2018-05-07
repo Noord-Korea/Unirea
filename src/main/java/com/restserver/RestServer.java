14498a478e8d1fd6200312aa5b76b66d33f1709b
bramk <bramkempen@gmail.com>
2018-05-07 11:28:56 +0200

    change username to email

--- src/main/java/com/restserver/RestServer.java
+++ src/main/java/com/restserver/RestServer.java
@@ -3,7 +3,6 @@ package com.restserver;
 import com.restserver.services.AccountService;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.FilterMapping;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
@@ -19,20 +18,23 @@ public class RestServer {
         context.setContextPath("/");
         Server jettyServer = new Server(8090);
 
+        //region Origin header
         FilterHolder cors = context.addFilter(CrossOriginFilter.class,"/*", EnumSet.of(DispatcherType.REQUEST));
         cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
         cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
         cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
         cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
-
+        //endregion
 
         jettyServer.setHandler(context);
         ServletHolder jerseyServlet =
                 context.addServlet(ServletContainer.class, "/*");
         jerseyServlet.setInitOrder(0);
+
         // Tells the Jersey Servlet which REST service/class to load.
         jerseyServlet.setInitParameter("jersey.config.server.provider.classnames",
                 AccountService.class.getCanonicalName());
+
         try {
             jettyServer.start();
             jettyServer.join();
