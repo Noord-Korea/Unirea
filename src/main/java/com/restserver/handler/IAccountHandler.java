50d3cdccce381ea508847d93a3399978fd235060
bramk <bramkempen@gmail.com>
2018-05-07 13:35:49 +0200

    add interface methods

--- src/main/java/com/restserver/handler/IAccountHandler.java
+++ src/main/java/com/restserver/handler/IAccountHandler.java
@@ -1,10 +1,20 @@
 package com.restserver.handler;
 
+import com.restserver.json.result.account.ChangePassword;
 import com.restserver.json.result.account.Login;
 import com.restserver.json.result.account.Register;
 
 public interface IAccountHandler {
     void Login(Login data);
+    void Logout();
+
     void Register(Register data);
-    void ChangePassword();
+    void ChangePassword(ChangePassword data);
+
+    void Update();
+
+    void HolidayReplacement();
+    void Delete();
+
+
 }
