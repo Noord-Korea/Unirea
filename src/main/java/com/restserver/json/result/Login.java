b6219c3c32b2f0ec8bd1e7491b4285d32c4b973d
bramk <bramkempen@gmail.com>
2018-05-07 10:37:50 +0200

    change username to email

--- src/main/java/com/restserver/json/result/Login.java
+++ src/main/java/com/restserver/json/result/Login.java
@@ -1,19 +1,20 @@
 package com.restserver.json.result;
 
 public class Login implements Result {
-    private String username;
+    private String email;
     private String password;
 
-    public Login(String username, String password) {
-        this.username = username;
+    public Login(String email, String password) {
+        this.email = email;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
         return password;
     }
+
 }
