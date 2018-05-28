55ae025d208b00f5fa396dfcb75e046987c843b8
bramk <bramkempen@gmail.com>
2018-05-28 15:13:02 +0200

    lalala

--- src/main/java/com/restserver/buildings/resource/factory/IResourceBuildingFactory.java
+++ src/main/java/com/restserver/buildings/resource/factory/IResourceBuildingFactory.java
@@ -1,4 +1,7 @@
 package com.restserver.buildings.resource.factory;
 
+import com.restserver.buildings.resource.models.IResourceBuilding;
+
 public interface IResourceBuildingFactory {
+    IResourceBuilding createBuilding();
 }
