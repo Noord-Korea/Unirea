cd0eeb7cab21b22e38373a54e0dfdcead2ac6707
bramk <bramkempen@gmail.com>
2018-05-28 13:59:32 +0200

    lalalla

--- src/main/java/com/restserver/factory/TownFactory.java
+++ src/main/java/com/restserver/factory/TownFactory.java
@@ -5,6 +5,10 @@ import com.models.Town;
 
 public class TownFactory {
     public static Town createTown(Player player){
+        if(player == null){
+            throw new IllegalArgumentException("Player is null");
+        }
+        
         return null;
     }
 }
