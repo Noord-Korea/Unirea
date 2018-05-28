6c89ac5b736fb44b279b9737160e7ad82baa0cf4
bramk <bramkempen@gmail.com>
2018-05-28 10:36:10 +0200

    add constructor

--- src/main/java/com/restserver/handler/TownHandler.java
+++ src/main/java/com/restserver/handler/TownHandler.java
@@ -1,6 +1,7 @@
 package com.restserver.handler;
 
 
+import com.dbal.repository.IRepository;
 import com.models.AccessToken;
 import com.models.Player;
 import com.restserver.json.request.town.BaseTownRequest;
@@ -8,6 +9,12 @@ import com.restserver.json.response.Reply;
 import com.restserver.json.response.Status;
 
 public class TownHandler implements ITownHandler {
+    private IRepository townRepository;
+
+    public TownHandler(IRepository townRepository) {
+        this.townRepository = townRepository;
+    }
+
     public Reply getTown(){
         return new Reply(Status.NotFound, "Not yet implemented");
     }
