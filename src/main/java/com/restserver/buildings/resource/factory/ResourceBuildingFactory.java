e69fba80a964adf64553db909e4020086a8bd4a9
stan martens <Stan Martens>
2018-05-28 16:40:28 +0200

    Fix error

--- src/main/java/com/restserver/buildings/resource/factory/ResourceBuildingFactory.java
+++ src/main/java/com/restserver/buildings/resource/factory/ResourceBuildingFactory.java
@@ -1,4 +1,11 @@
 package com.restserver.buildings.resource.factory;
 
+import com.restserver.buildings.resource.models.IResourceBuilding;
+
 public class ResourceBuildingFactory implements IResourceBuildingFactory {
+
+    @Override
+    public IResourceBuilding createBuilding() {
+        return null;
+    }
 }
