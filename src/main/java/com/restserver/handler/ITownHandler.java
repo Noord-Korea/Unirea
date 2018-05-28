6a879102368a97612e0e322c16b7334e9cad588d
bramk <bramkempen@gmail.com>
2018-05-28 10:28:57 +0200

    add town start

--- src/main/java/com/restserver/handler/ITownHandler.java
+++ src/main/java/com/restserver/handler/ITownHandler.java
@@ -1,7 +1,9 @@
 package com.restserver.handler;
 
+import com.restserver.json.request.town.BaseTownRequest;
 import com.restserver.json.response.Reply;
 
 public interface ITownHandler {
     Reply getTown();
+    Reply createTown(BaseTownRequest baseTownRequest);
 }
--- src/main/java/com/restserver/handler/TownHandler.java
+++ src/main/java/com/restserver/handler/TownHandler.java
@@ -1,11 +1,20 @@
 package com.restserver.handler;
 
 
+import com.models.AccessToken;
+import com.models.Player;
+import com.restserver.json.request.town.BaseTownRequest;
 import com.restserver.json.response.Reply;
 import com.restserver.json.response.Status;
 
 public class TownHandler implements ITownHandler {
     public Reply getTown(){
         return new Reply(Status.NotFound, "Not yet implemented");
+    }
+
+    @Override
+    public Reply createTown(BaseTownRequest baseTownRequest) {
+        AccessToken accessToken = baseTownRequest.getToken();
+        return null;
     }
 }
