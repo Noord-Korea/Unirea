18dfb88a7a1488da1a41d0e49265755bb54080d1
bramk <bramkempen@gmail.com>
2018-06-04 13:54:58 +0200

    add map service

---
+++ src/main/java/com/restserver/handler/IMapHandler.java
@@ -0,0 +1,10 @@
+package com.restserver.handler;
+
+import com.restserver.json.response.town.Map;
+import com.restserver.json.response.town.TownPosition;
+
+import java.util.List;
+
+public interface IMapHandler {
+    List<TownPosition> getAllTowns();
+}
