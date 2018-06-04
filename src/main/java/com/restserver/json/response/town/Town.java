e76a6c5c38fc0d6e7c6c8058b5c1a895ed86958d
bramk <bramkempen@gmail.com>
2018-06-04 10:34:57 +0200

    add setters

--- src/main/java/com/restserver/json/response/town/Town.java
+++ src/main/java/com/restserver/json/response/town/Town.java
@@ -49,4 +49,28 @@ public class Town {
     public int getY() {
         return y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setTownResources(Set<TownResources> townResources) {
+        this.townResources = townResources;
+    }
+
+    public void setTownBuildings(Set<TownBuilding> townBuildings) {
+        this.townBuildings = townBuildings;
+    }
+
+    public void setBuildingQueues(Set<BuildingQueue> buildingQueues) {
+        this.buildingQueues = buildingQueues;
+    }
+
+    public void setTownArmies(Set<TownArmy> townArmies) {
+        this.townArmies = townArmies;
+    }
 }
