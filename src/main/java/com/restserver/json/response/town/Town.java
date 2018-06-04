7583120f0a3a121acde1518a027b511bb4163c1c
bramk <bramkempen@gmail.com>
2018-06-04 10:54:39 +0200

    remove stuff

--- src/main/java/com/restserver/json/response/town/Town.java
+++ src/main/java/com/restserver/json/response/town/Town.java
@@ -13,15 +13,9 @@ public class Town {
     private Set<TownResources> townResources = new HashSet<TownResources>(0);
     private Set<TownBuilding> townBuildings = new HashSet<>(0);
 
-    private Set<BuildingQueue> buildingQueues = new HashSet<>(0);
-
-    private Set<TownArmy> townArmies = new HashSet<>(0);
-
-    public Town(Set<TownResources> townResources, Set<TownBuilding> townBuildings, Set<BuildingQueue> buildingQueues, Set<TownArmy> townArmies, int x, int y) {
+    public Town(Set<TownResources> townResources, Set<TownBuilding> townBuildings, int x, int y) {
         this.townResources = townResources;
         this.townBuildings = townBuildings;
-        this.buildingQueues = buildingQueues;
-        this.townArmies = townArmies;
         this.x = x;
         this.y = y;
     }
@@ -32,14 +26,6 @@ public class Town {
 
     public Set<TownBuilding> getTownBuildings() {
         return townBuildings;
-    }
-
-    public Set<BuildingQueue> getBuildingQueues() {
-        return buildingQueues;
-    }
-
-    public Set<TownArmy> getTownArmies() {
-        return townArmies;
     }
 
     public int getX() {
@@ -64,13 +50,5 @@ public class Town {
 
     public void setTownBuildings(Set<TownBuilding> townBuildings) {
         this.townBuildings = townBuildings;
-    }
-
-    public void setBuildingQueues(Set<BuildingQueue> buildingQueues) {
-        this.buildingQueues = buildingQueues;
-    }
-
-    public void setTownArmies(Set<TownArmy> townArmies) {
-        this.townArmies = townArmies;
     }
 }
