a1a3ca471a97a5228bb5bcbc51d049a9b5b132ad
bramk <bramkempen@gmail.com>
2018-06-04 10:57:19 +0200

    fix random

--- src/main/java/com/models/Town.java
+++ src/main/java/com/models/Town.java
@@ -122,7 +122,7 @@ public class Town {
 
         this.player = player;
         this.name = name;
-        this.x = r.nextInt()*1000;
-        this.y = r.nextInt()*1000;
+        this.x = r.nextInt(1000);
+        this.y = r.nextInt(1000);
     }
 }
