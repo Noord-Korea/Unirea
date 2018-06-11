7ad37a58c8b582b4076e0829523bd86aef739050
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-06-11 13:08:16 +0200

    Cleaned Buildinghandler

--- src/main/java/com/restserver/handler/BuildingHandler.java
+++ src/main/java/com/restserver/handler/BuildingHandler.java
@@ -7,6 +7,7 @@ import com.models.Building;
 import com.models.Town;
 import com.models.TownBuilding;
 import com.restserver.buildings.resource.ResourceType;
+import com.restserver.buildings.resource.models.BaseResourceBuilding;
 import com.restserver.buildings.resource.models.IronBuilding;
 import com.restserver.buildings.resource.models.OilBuilding;
 import com.restserver.buildings.resource.models.WoodBuilding;
@@ -18,6 +19,33 @@ public class BuildingHandler {
     TownBuildingRepository townBuildingRepository = new TownBuildingRepository();
     BuildingRepository buildingRepository = new BuildingRepository();
     TownRepository townRepository = new TownRepository();
+
+    public TownBuilding getNormalTownBuildings(int townId, int buildingId){
+        switch (buildingId){
+            //HEADQUARTERS
+            case 4:
+                return null;
+            //WAREHOUSE
+            case 5:
+                return null;
+            //BARRACKS
+            case 6:
+                return null;
+            //TRAINING GROUNDS
+            case 7:
+                return null;
+            //FOUNDRY
+            case 8:
+                return null;
+            //AMMUNITION DEPOT
+            case 9:
+                return null;
+            //WALL
+            case 10:
+                return null;
+        }
+        return null;
+    }
     
     public Building getResourceBuilding(int townId, int buildingId){
         
@@ -31,38 +59,29 @@ public class BuildingHandler {
             //OIL
             case 1:
                 OilBuilding oilBuilding = new OilBuilding();
-                for (TownBuilding townBuilding : townBuildings){
-                    if(townBuilding.getBuilding().getId() == 1){
-                        oilBuilding.setName(townBuilding.getBuilding().getName());
-                        oilBuilding.setBuildingLevel(townBuilding.getLevel());
-                        oilBuilding.setResourceType(ResourceType.OIL);
-                    }
-                }
-                return oilBuilding;
+                return constructResourceBuilding(townBuildings, oilBuilding);
             //IRON
             case 2:
                 IronBuilding ironBuilding = new IronBuilding();
-                for (TownBuilding townBuilding : townBuildings){
-                    if(townBuilding.getBuilding().getId() == 2){
-                        ironBuilding.setName(townBuilding.getBuilding().getName());
-                        ironBuilding.setBuildingLevel(townBuilding.getLevel());
-                        ironBuilding.setResourceType(ResourceType.OIL);
-                    }
-                }
-                return ironBuilding;
+                return constructResourceBuilding(townBuildings, ironBuilding);
             //WOOD
             case 3:
                 WoodBuilding woodBuilding = new WoodBuilding();
-                for (TownBuilding townBuilding : townBuildings){
-                    if(townBuilding.getBuilding().getId() == 3){
-                        woodBuilding.setName(townBuilding.getBuilding().getName());
-                        woodBuilding.setBuildingLevel(townBuilding.getLevel());
-                        woodBuilding.setResourceType(ResourceType.OIL);
-                    }
-                }
-                return woodBuilding;
+                return constructResourceBuilding(townBuildings, woodBuilding);
         }
         return null;
     }
+
+    private Building constructResourceBuilding(Set<TownBuilding> townBuildings, BaseResourceBuilding building) {
+        for (TownBuilding townBuilding : townBuildings){
+            if(townBuilding.getBuilding().getId() == 3){
+                building.setName(townBuilding.getBuilding().getName());
+                building.setBuildingLevel(townBuilding.getLevel());
+                building.setResourceType(ResourceType.WOOD);
+            }
+        }
+        return building;
+    }
+
 
 }
