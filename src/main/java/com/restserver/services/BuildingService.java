bce4c40f2b8f3178fa4ab21dbe319b0219630cc4
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-06-21 12:18:39 +0200

    Implemented BuildingTicker

--- src/main/java/com/restserver/services/BuildingService.java
+++ src/main/java/com/restserver/services/BuildingService.java
@@ -3,6 +3,10 @@ package com.restserver.services;
 import com.google.gson.Gson;
 import com.restserver.handler.BuildingHandler;
 import com.restserver.json.request.building.LevelUp;
+import com.restserver.json.response.Reply;
+import com.restserver.json.response.Status;
+import com.restserver.utils.accesstoken.AccessTokenLevel;
+import com.restserver.utils.accesstoken.AccessTokenUtil;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -16,8 +20,18 @@ public class BuildingService {
     @Consumes("application/json")
     @Path("/levelup")
     public void levelUp(String data) {
+        Reply reply = null;
         Gson gson = new Gson();
         LevelUp levelUp = gson.fromJson(data, LevelUp.class);
-        handler.levelUp(levelUp);
+        if (!AccessTokenUtil.checkAccess(levelUp.getToken(), AccessTokenLevel.LOGGEDIN)) {
+            reply = new Reply(Status.NOAUTH, "Accesstoken not valid");
+        }
+        if (reply == null) {
+            reply = handler.levelUp(levelUp);
+        }
+
+        if (reply == null) {
+            reply = new Reply(Status.ERROR, "Something went wrong");
+        }
     }
 }
