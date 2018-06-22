7fdfb371a0d77df201cdcb10f5255eac187be2f6
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-06-22 12:46:18 +0200

    Buildingtick finished and working

--- src/main/java/com/server/tick/BuildingTick.java
+++ src/main/java/com/server/tick/BuildingTick.java
@@ -1,11 +1,14 @@
 package com.server.tick;
 
+import com.dbal.repository.BuildingQueueRepository;
 import com.dbal.repository.BuildingRepository;
+import com.dbal.repository.TownBuildingRepository;
 import com.dbal.repository.TownRepository;
 import com.logging.LogLevel;
 import com.logging.Logger;
 import com.models.BuildingQueue;
 import com.models.Town;
+import com.models.TownBuilding;
 import com.restserver.buildings.resource.models.BaseNormalBuilding;
 import com.restserver.buildings.resource.models.BaseResourceBuilding;
 import com.restserver.handler.BuildingHandler;
@@ -14,39 +17,48 @@ import com.restserver.handler.IBuildingHandler;
 import java.util.List;
 
 public class BuildingTick implements Runnable {
-    public void update() {
+    public void run() {
         Logger.getInstance().log("BuildingTick Running", LogLevel.INFORMATION);
         BuildingRepository buildingRepository = new BuildingRepository();
+        BuildingQueueRepository buildingQueueRepository = new BuildingQueueRepository();
+        TownBuildingRepository townBuildingRepository = new TownBuildingRepository();
         TownRepository townRepository = new TownRepository();
         List<Town> towns = townRepository.findAllNoDuplicates(null);
         IBuildingHandler buildingHandler = new BuildingHandler();
+        boolean removeQueue = false;
         for (Town town : towns) {
             if (town.getBuildingQueues().isEmpty()) {
                 continue;
             } else {
                 List<BuildingQueue> queues = town.getBuildingQueues();
                 for (BuildingQueue queue : queues) {
-                    if (queue.getValue() == 0){
+                    if (queue.getValue() <= 0){
                         int buildingId = queue.getPk().getBuilding().getId();
                         if (buildingId > 3) {
-                            BaseNormalBuilding building = (BaseNormalBuilding) buildingHandler.getNormalTownBuildings(town.getId(), buildingId);
-                            building.setBuildingLevel(building.getBuildingLevel() + 1);
-                            buildingRepository.save(building);
+                            for (TownBuilding townBuilding : town.getTownBuildings()){
+                                if (townBuilding.getBuilding().getId() == buildingId){
+                                    townBuilding.setLevel(townBuilding.getLevel() + 1);
+                                    townBuildingRepository.save(townBuilding);
+
+                                }
+                            }
                         } else {
-                            BaseResourceBuilding building = (BaseResourceBuilding) buildingHandler.getResourceBuilding(town.getId(), buildingId);
-                            building.setBuildingLevel(building.getBuildingLevel() + 1);
-                            buildingRepository.save(building);
+                            for (TownBuilding townBuilding : town.getTownBuildings()){
+                                if (townBuilding.getBuilding().getId() == buildingId){
+                                    townBuilding.setLevel(townBuilding.getLevel() + 1);
+                                    townBuildingRepository.save(townBuilding);
+                                }
+                            }
                         }
+                        buildingQueueRepository.delete(queue);
+                        removeQueue = true;
                     }
                     queue.setValue(queue.getValue() - 5);
+                    if (!removeQueue){
+                        buildingQueueRepository.save(queue);
+                    }
                 }
             }
         }
-    }
-
-    @Override
-    public void run() {
-        Logger.getInstance().log("BuildingTick Running", LogLevel.INFORMATION);
-
     }
 }
