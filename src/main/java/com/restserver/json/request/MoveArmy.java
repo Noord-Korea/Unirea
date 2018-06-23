caa1f1513d0a00855893156f4e2f4daf3dd71449
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-06-23 21:46:30 +0200

    changed movementhandler to allow arraylist instead of map

--- src/main/java/com/restserver/json/request/MoveArmy.java
+++ src/main/java/com/restserver/json/request/MoveArmy.java
@@ -7,14 +7,12 @@ import java.util.Map;
 public class MoveArmy {
     private ArrayList<Integer> troopAmount;
     private int targetTownId;
-    private int value;
     private int townId;
     private String token;
 
     public MoveArmy(ArrayList<Integer> troopAmount, int targetTownId, int value, int townId, String token) {
         this.troopAmount = troopAmount;
         this.targetTownId = targetTownId;
-        this.value = value;
         this.townId = townId;
         this.token = token;
     }
@@ -25,14 +23,6 @@ public class MoveArmy {
 
     public void setTargetTownId(int targetTownId) {
         this.targetTownId = targetTownId;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
     }
 
     public int getTownId() {
