f48026d3a01595ce2660955cad5ee883c7899ac3
stan martens <stan-martens@hotmail.com>
2018-06-24 13:28:19 +0200

    Change list to arraylist

--- src/main/java/com/restserver/handler/ArmyMovementHandler.java
+++ src/main/java/com/restserver/handler/ArmyMovementHandler.java
@@ -76,7 +76,7 @@ public class ArmyMovementHandler implements IArmyMovementHandler {
         queue.setGoingHome(false);
         int time = queue.getValue() * 2;
 
-        List<ArmyMovementQueue> queues = new ArrayList<>(homeTown.getArmyMovementQueues());
+        ArrayList<ArmyMovementQueue> queues = new ArrayList<>(homeTown.getArmyMovementQueues());
         queues.add(queue);
         armyMovementQueueRepository.save(queues);
 
